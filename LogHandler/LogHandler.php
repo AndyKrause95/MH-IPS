ad101afcf64cf466b3a452ad89ffb595347749fb
Andy <Andy@MHNB160.fritz.box>
2018-03-14 00:55:33 +0100

    LogHandler

--- LogHandler/LogHandler.php
+++ LogHandler/LogHandler.php
@@ -15,16 +15,21 @@ class LogHandler {
 	/**
 	 */
 	public function __construct() {
+		$this->logFilePointer = fopen ( $this->logFilePath . $this->logFileName, "a+" );
+		
+		if ($this->logFilePointer === false) {
+			echo "LogHandler: File '. $this->logFilePath . $this->logFileName .' could not be created \n";
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	/**
 	 */
-	public function writeToLog($message) {
-	}
-	
-	/**
-	 */
-	private function writeMessageToLog($message) {
+	public function writeToLog($timestamp, $severity, $file, $line, $type, $message) {
+		$timeFormatted = gmdate("Y-m-d H:i:s e", $timestamp);
+		$errorMessage = $timeFormatted . "\t-\t" . $severity . "\t-\t" . $message . "\n";
 	}
 }
 
