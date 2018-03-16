ac92c6d3b4eb1050030499d3821fe2ee64daee3a
AndyKrause95 <andre.krause95@gmail.com>
2018-03-16 07:29:09 +0100

    Logging Changes

--- LogHandler/LogHandler.php
+++ LogHandler/LogHandler.php
@@ -9,9 +9,7 @@ namespace LogHandler;
  */
 class LogHandler {
 	
-	/**
-	 * Log Levels
-	 */
+	// LOG LEVELS
 	const LOGLEVEL_OFF = 0;
 	const LOGLEVEL_DEBUG = 1;
 	const LOGLEVEL_ERROR = 2;
@@ -19,8 +17,29 @@ class LogHandler {
 	const LOGLEVEL_ALL = 5;
 	
 	/**
+	 *
+	 * @var integer Current active Log Level
 	 */
-	public function __construct() {
+	private $logLevelActive = 5;
+	
+	/**
+	 *
+	 * @param integer $logLevelActive
+	 */
+	public function __construct($logLevelActive = 5) {
+		$this->logLevelActive = $logLevelActive;
+	}
+	
+	/**
+	 *
+	 * @param string $message
+	 * @param integer $level
+	 */
+	public function logMessage($message, $level) {
+		// TODO - Implement Logging to file and display
+		if ($level == $this->logLevelActive) {
+		} else {
+		}
 	}
 	
 	/**
